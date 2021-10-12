99f7aa74ea273dd9e490321c8afb2eaf2a0a97fc
jérémy angele <jeremyangele@gmail.com>
2021-10-12 16:32:07 -0400

    Implementation of the vertical navbar

---
+++ sportsee/src/Home/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render(){
+        return(
+            <section className="content">
+                <nav className="navbar-left">
+                    <ul className="navbar-icons">
+                        <li>tata</li>
+                        <li>tata</li>
+                    </ul>
+                </nav>
+                <div className="content">
+
+                </div>
+            </section>
+        )
+    }
+}
+
+export default Home;
